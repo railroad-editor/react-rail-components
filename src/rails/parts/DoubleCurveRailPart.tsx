459bb29d07c1299e4daddd0ed2e38ee8f038dcbf
tozawa <kota65535@gmail.com>
2019-07-29 01:27:06 +0900

    add sources

---
+++ src/rails/parts/DoubleCurveRailPart.tsx
@@ -0,0 +1,91 @@
+import * as React from "react";
+import {RAIL_PART_WIDTH, RAIL_SPACE} from "constants";
+import {Pivot} from "./primitives/PartBase";
+import RailPartBase, {RailPartBaseDefaultProps, RailPartBaseProps} from "./RailPartBase";
+
+import {ArcDirection, default as ArcPart} from "./primitives/ArcPart";
+
+
+
+interface DoubleCurveRailPartProps extends RailPartBaseProps {
+  innerRadius: number
+  outerRadius: number
+  centerAngle: number
+  direction: ArcDirection
+}
+
+
+export default class DoubleCurveRailPart extends RailPartBase<DoubleCurveRailPartProps, {}> {
+  public static defaultProps: RailPartBaseDefaultProps = RailPartBase.defaultProps
+
+  constructor(props: DoubleCurveRailPartProps) {
+    super(props)
+  }
+
+  get joints() {
+    return [
+      {pivotPartIndex: 0, pivot: Pivot.LEFT},
+      {pivotPartIndex: 0, pivot: Pivot.RIGHT},
+      {pivotPartIndex: 1, pivot: Pivot.LEFT},
+      {pivotPartIndex: 1, pivot: Pivot.RIGHT}
+    ]
+  }
+
+  get feederSockets() {
+    return [
+      {pivotPartIndex: 0, pivot: Pivot.CENTER},
+      {pivotPartIndex: 1, pivot: Pivot.CENTER},
+    ]
+  }
+
+  get conductiveParts() {
+    return [0, 1]
+  }
+
+
+  renderParts = () => {
+    const {innerRadius, outerRadius, centerAngle, direction, fillColor, flowDirections} = this.props
+
+    let radius1, radius2
+    switch (this.props.direction) {
+      case ArcDirection.RIGHT:
+        radius1 = outerRadius
+        radius2 = innerRadius
+        break
+      case ArcDirection.LEFT:
+        radius1 = innerRadius
+        radius2 = outerRadius
+        break
+    }
+
+    return (
+      <>
+        <ArcPart
+          pivot={Pivot.LEFT}
+          direction={direction}
+          radius={radius1}
+          centerAngle={centerAngle}
+          width={RAIL_PART_WIDTH}
+          fillColor={fillColor}
+          flowDirection={flowDirections[0]}
+          data={{
+            type: 'Part'
+          }}
+        />
+        <ArcPart
+          pivot={Pivot.LEFT}
+          direction={direction}
+          position={{x: 0, y: RAIL_SPACE}}
+          radius={radius2}
+          centerAngle={centerAngle}
+          width={RAIL_PART_WIDTH}
+          fillColor={fillColor}
+          flowDirection={flowDirections[1]}
+          data={{
+            type: 'Part'
+          }}
+        />
+      </>
+    )
+  }
+}
---
+++ src/rails/parts/SimpleTurnoutRailPart.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import RectPart from "./primitives/RectPart";
+import ArcPart, {ArcDirection} from "./primitives/ArcPart";
+import {RAIL_PART_WIDTH} from "constants";
+import {Pivot} from "./primitives/PartBase";
+import RailPartBase, {RailPartBaseDefaultProps, RailPartBaseProps} from "./RailPartBase";
+
+
+
+
+
+interface SimpleTurnoutRailPartProps extends RailPartBaseProps {
+  length: number
+  radius: number
+  centerAngle: number
+  direction: ArcDirection
+}
+
+
+export default class SimpleTurnoutRailPart extends RailPartBase<SimpleTurnoutRailPartProps, {}> {
+  public static defaultProps: RailPartBaseDefaultProps = RailPartBase.defaultProps
+
+  constructor(props: SimpleTurnoutRailPartProps) {
+    super(props)
+  }
+
+  get joints() {
+    return [
+      [
+        {pivotPartIndex: 0, pivot: Pivot.LEFT},
+        {pivotPartIndex: 0, pivot: Pivot.RIGHT},
+        {pivotPartIndex: 1, pivot: Pivot.RIGHT}
+      ],
+      [
+        {pivotPartIndex: 1, pivot: Pivot.LEFT},
+        {pivotPartIndex: 0, pivot: Pivot.RIGHT},
+        {pivotPartIndex: 1, pivot: Pivot.RIGHT}
+      ]
+    ][this.props.conductionState]
+  }
+
+  get feederSockets() {
+    return []
+  }
+
+  get conductiveParts() {
+    return [[0], [1]][this.props.conductionState]
+  }
+
+
+  renderParts = () => {
+    const {length, radius, centerAngle, direction, fillColor, flowDirections} = this.props
+
+    return (
+      <>
+        <RectPart
+          width={RAIL_PART_WIDTH}
+          height={length}
+          pivot={Pivot.LEFT}
+          fillColor={fillColor}
+          flowDirection={flowDirections[0]}
+          data={{
+            type: 'Part'
+          }}
+        />
+        <ArcPart
+          direction={direction}
+          radius={radius}
+          centerAngle={centerAngle}
+          width={RAIL_PART_WIDTH}
+          pivot={Pivot.LEFT}
+          fillColor={fillColor}
+          flowDirection={flowDirections[1]}
+          data={{
+            type: 'Part'
+          }}
+        />
+      </>
+    )
+  }
+}
